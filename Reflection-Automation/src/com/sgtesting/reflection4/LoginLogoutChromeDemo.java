49930c64adc62704f6817450e960e318ca4a9877
Ranjini14 <devarajuranjini@gmail.com>
2021-11-11 16:07:27 +0530

    Assignment1

---
+++ Reflection-Automation/src/com/sgtesting/reflection4/LoginLogoutChromeDemo.java
@@ -0,0 +1,91 @@
+package com.sgtesting.reflection4;
+
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginLogoutChromeDemo {
+	public static WebDriver oBrowser=null;
+	public static void main(String[] args) {
+		
+	}
+	static void launchBrowser()
+	{
+		try
+		{
+			System.setProperty("webdriver.chrome.driver", "E:\\Example Automation\\Automation\\Web-Automation\\Library\\drivers\\chromedriver.exe");
+			oBrowser=new ChromeDriver();
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+     public void navigate()
+	{
+		try
+		{
+			oBrowser.navigate().to("http://localhost:82/login.do");
+			oBrowser.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(60));
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+	
+	public void login()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("username")).sendKeys("admin");
+			oBrowser.findElement(By.name("pwd")).sendKeys("manager");
+			oBrowser.findElement(By.xpath("//*[@id='loginButton']/div")).click();
+			Thread.sleep(4000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+	
+	public void minimizeFlyOutWindow()
+	{
+		try
+		{
+			oBrowser.findElement(By.id("gettingStartedShortcutsPanelId")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+	
+	public void logout()
+	{
+		try
+		{
+			oBrowser.findElement(By.linkText("Logout")).click();
+			Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+	
+	//public void closeApplication()
+	{
+		try
+		{
+			oBrowser.close();
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+}
+
+
